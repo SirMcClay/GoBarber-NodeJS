943c9b40f3dfdfd092d55e48bea67701c3efa320
sirmcclay <mcclay.design@gmail.com>
2020-06-25 21:05:23 -0300

    config, route, appointment...

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,10 @@
-import express, { response } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json(user);
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
